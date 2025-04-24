291adb53f23406bb71501869dc193aba0c84438a
ContactCiss <richard@contactons.nl>
2025-04-24 16:08:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,15 @@ from twilio.twiml.voice_response import VoiceResponse, Dial
 
 app = Flask(__name__)
 
+# Voeg deze regel toe om 404 niet te krijgen op de root route
+@app.route("/", methods=["GET"])
+def home():
+    return "Flask is up and running"
+
 @app.route("/doorverbinden", methods=["POST"])
 def doorverbinden():
     response = VoiceResponse()
-    # Dit bericht moet aan de beller worden verteld
     response.say("Een ogenblikje, ik verbind u nu door naar een medewerker.", language='nl-NL')
-    
-    # Het daadwerkelijke doorverbinden naar het juiste nummer
-    response.dial("0031884114114", caller_id="+3197010252180")  # Twilio-nummer als caller_id
-    return Response(str(response), mimetype='text/xml')
+    response.dial("0031884114114", caller_id="+3197010252180")  # Je Twilio-nummer
+    return Response(str(response), mimetype='
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-
-print(f"POST-request ontvangen: {request.json}")
